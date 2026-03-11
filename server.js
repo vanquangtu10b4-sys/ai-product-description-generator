904fd4e3bcac27d0fddaa00618c026f3a39aabe8
vanquangtu10b4-sys <vanquangtu10b4@gmail.com>
2026-03-11 14:32:43 +0700

    fix azure port

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.post("/generate", (req, res) => {
     })
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT)
